3f11a90ae4036ca86f345320beed4fbd10079e5b
nxtrohith <avsrohith.06@gmail.com>
2025-12-31 11:41:59 +0530

    Add 'use client' directive to Loading component for proper client-side rendering

--- app/event/[slug]/loading.tsx
+++ app/event/[slug]/loading.tsx
@@ -1,3 +1,4 @@
+'use client';
 export default function Loading() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-gray-900 to-black">
